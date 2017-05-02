38afc182d9d2b5332413b51139aaeab013e6d45f
Stuart Terrett <shterrett@gmail.com>
2017-05-02 10:27:08 -0400

    Only print matching server id

--- src/Server.hs
+++ src/Server.hs
@@ -2,29 +2,49 @@ module Server
   (runServer
   ) where
 
+import qualified Data.Map.Strict as Map
 import ConcurrentUtils
 import Control.Monad
+import Control.Monad.STM
+import Control.Concurrent.STM.TVar
 import Network
 import System.IO
 import Models
 import Messages (decode, encode)
 
 runServer :: ServerId -> CurrentConnection -> ConnectionDB -> MessageQueues -> IO ()
-runServer selfId _ _ _ = do
+runServer selfId conn db qs = do
   sock <- listenOn defaultPort
   forever $ do
      (handle, host, port) <- accept sock
-     forkFinally (talk selfId handle) (\_ -> hClose handle)
+     forkFinally (talk selfId handle conn db qs) (\_ -> hClose handle)
 
-talk :: ServerId -> Handle -> IO ()
-talk selfId h = do
+talk :: ServerId -> Handle -> CurrentConnection -> ConnectionDB -> MessageQueues -> IO ()
+talk selfId h conn db qs = do
     hSetBuffering h LineBuffering
     forever $ do
-      hGetLine h >>= (handleMessage selfId h)
+      hGetLine h >>= (handleMessage selfId h conn db qs)
 
-handleMessage :: ServerId -> Handle -> String -> IO ()
-handleMessage selfId h msg =
+handleMessage :: ServerId -> Handle -> CurrentConnection -> ConnectionDB -> MessageQueues -> String -> IO ()
+handleMessage selfId h conn db qs msg =
     case decode msg of
       Just (Handshake id) -> hPutStrLn h (encode $ Handshake selfId)
-      Just (Msg id msg) -> putStrLn msg
+      Just (Msg id msg) -> printOrQueue conn db qs id msg
       Nothing -> return ()
+
+printOrQueue :: CurrentConnection -> ConnectionDB -> MessageQueues -> ServerId -> String -> IO ()
+printOrQueue conn db qs id msg = do
+    currId <- currentServerId conn db
+    case currId of
+      Just cid -> if cid == id
+                  then putStrLn msg
+                  else return ()
+      Nothing -> return ()
+
+currentServerId :: CurrentConnection -> ConnectionDB -> IO (Maybe ServerId)
+currentServerId conn db =
+    currentConnectionName >>=
+      (\mn -> join <$> sequence (connectionId <$> mn))
+  where
+    currentConnectionName = atomically $ (fmap fst) <$> readTVar conn :: IO (Maybe ConnectionName)
+    connectionId name = atomically $ (\map -> (Map.lookup name map) >>= serverId) <$> readTVar db :: IO (Maybe ServerId)
